7a02f42818b1704c6a89c10059632790551bddb0
SampleTrack <FirstSample@proton.me>
2026-05-27 19:05:40 +0530

    Use persistent MongoDB queue

--- bot/plugins/deal.py
+++ bot/plugins/deal.py
@@ -2,8 +2,8 @@ from pyrogram import filters
 from pyrogram.types import Message
 from bot.core.client import bot
 from bot.config import Config
-from bot.core.queue import post_queue
 from bot.utils.template import build_deal_post
+from bot.database.queue_db import add_queue
 
 @bot.on_message(filters.command('deal') & filters.user(Config.OWNER_ID))
 async def add_deal(client, message: Message):
@@ -18,9 +18,9 @@ async def add_deal(client, message: Message):
 
     text = build_deal_post(title, price, link)
 
-    await post_queue.put({
+    await add_queue({
         'link': link,
         'text': text
     })
 
-    await message.reply_text('✅ Deal Added To Queue')
+    await message.reply_text('✅ Deal Saved To Persistent Queue')
