80ffd21494fe741c890f38ab4dcf34274b7d019f
SampleTrack <FirstSample@proton.me>
2026-05-27 19:02:10 +0530

    Upgrade deal plugin with templates and buttons

--- bot/plugins/deal.py
+++ bot/plugins/deal.py
@@ -3,16 +3,20 @@ from pyrogram.types import Message
 from bot.core.client import bot
 from bot.config import Config
 from bot.core.queue import post_queue
+from bot.utils.template import build_deal_post
 
 @bot.on_message(filters.command('deal') & filters.user(Config.OWNER_ID))
 async def add_deal(client, message: Message):
-    if len(message.command) < 3:
+    if len(message.command) < 4:
         return await message.reply_text(
-            'Usage: /deal link message'
+            'Usage: /deal title price link'
         )
 
-    link = message.command[1]
-    text = ' '.join(message.command[2:])
+    title = message.command[1]
+    price = message.command[2]
+    link = message.command[3]
+
+    text = build_deal_post(title, price, link)
 
     await post_queue.put({
         'link': link,
