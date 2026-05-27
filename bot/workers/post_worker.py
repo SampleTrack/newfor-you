98b03958ba65562daae51e4e5514cd5358e92b6a
SampleTrack <FirstSample@proton.me>
2026-05-27 19:05:55 +0530

    Convert worker to persistent MongoDB queue

--- bot/workers/post_worker.py
+++ bot/workers/post_worker.py
@@ -1,45 +1,53 @@
 import asyncio
 from pyrogram.errors import FloodWait
 from pyrogram.enums import ParseMode
-from bot.core.queue import post_queue
 from bot.core.client import bot
 from bot.database.mongo import channels
 from bot.utils.duplicate import is_duplicate, save_post
 from bot.utils.logger import logger
 from bot.utils.buttons import deal_button
+from bot.database.queue_db import (
+    get_pending_deals,
+    mark_posted,
+    mark_failed
+)
 
 async def post_worker():
     while True:
-        deal = await post_queue.get()
+        deals = get_pending_deals()
 
-        try:
-            link = deal.get('link')
-            text = deal.get('text')
+        async for deal in deals:
+            try:
+                link = deal.get('link')
+                text = deal.get('text')
 
-            if await is_duplicate(link):
-                logger.info('Duplicate deal skipped')
-                continue
+                if await is_duplicate(link):
+                    logger.info('Duplicate deal skipped')
+                    await mark_posted(deal['_id'])
+                    continue
 
-            async for channel in channels.find({}):
-                try:
-                    await bot.send_message(
-                        channel['channel_id'],
-                        text,
-                        parse_mode=ParseMode.HTML,
-                        reply_markup=deal_button(link),
-                        disable_web_page_preview=False
-                    )
+                async for channel in channels.find({}):
+                    try:
+                        await bot.send_message(
+                            channel['channel_id'],
+                            text,
+                            parse_mode=ParseMode.HTML,
+                            reply_markup=deal_button(link),
+                            disable_web_page_preview=False
+                        )
 
-                except FloodWait as e:
-                    logger.warning(f'FloodWait: {e.value}')
-                    await asyncio.sleep(e.value)
+                    except FloodWait as e:
+                        logger.warning(f'FloodWait: {e.value}')
+                        await asyncio.sleep(e.value)
 
-                except Exception as err:
-                    logger.error(str(err))
+                    except Exception as err:
+                        logger.error(str(err))
 
-            await save_post(link)
+                await save_post(link)
+                await mark_posted(deal['_id'])
 
-        except Exception as e:
-            logger.error(str(e))
+            except Exception as e:
+                logger.error(str(e))
+                await mark_failed(deal['_id'], str(e))
 
-        post_queue.task_done()
+        await asyncio.sleep(10)
